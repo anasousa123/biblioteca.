5066afc69ef071bb966405efb96c36a42f15b393
anasousa123 <ana.7410062@aluno.mg.gov.br>
2026-04-27 07:31:59 -0300

    k

---
+++ alunos.js
@@ -0,0 +1,50 @@
+import { db } from "./firebase.js";
+import { collection, addDoc, getDocs, deleteDoc, doc } 
+from "https://www.gstatic.com/firebasejs/12.12.1/firebase-firestore.js";
+
+const form = document.getElementById("formAluno");
+const tabela = document.getElementById("tabela");
+
+// CADASTRAR
+form.addEventListener("submit", async (e)=>{
+e.preventDefault();
+
+await addDoc(collection(db,"alunos"),{
+nome:nome.value,
+turma:turma.value,
+nivel:nivel.value,
+email:email.value
+});
+
+form.reset();
+carregar();
+});
+
+// LISTAR
+async function carregar(){
+tabela.innerHTML = "";
+
+const dados = await getDocs(collection(db,"alunos"));
+
+dados.forEach((item)=>{
+tabela.innerHTML += `
+<tr>
+<td>${item.data().nome}</td>
+<td>${item.data().turma}</td>
+<td>${item.data().nivel}</td>
+<td>${item.data().email}</td>
+<td>
+<button onclick="remover('${item.id}')" class="btn btn-danger btn-sm">Excluir</button>
+</td>
+</tr>
+`;
+});
+}
+
+// EXCLUIR
+window.remover = async(id)=>{
+await deleteDoc(doc(db,"alunos",id));
+carregar();
+};
+
+carregar();
---
+++ emprestimo.js
@@ -0,0 +1,83 @@
+import { db } from "./firebase.js";
+import {
+collection, addDoc, getDocs, updateDoc, doc
+} from "https://www.gstatic.com/firebasejs/12.12.1/firebase-firestore.js";
+
+const selAluno = document.getElementById("aluno");
+const selLivro = document.getElementById("livro");
+
+async function carregarSelects(){
+
+// alunos
+let alunos = await getDocs(collection(db,"alunos"));
+selAluno.innerHTML = "<option>Aluno</option>";
+alunos.forEach(a=>{
+selAluno.innerHTML += `<option value="${a.id}">${a.data().nome}</option>`;
+});
+
+// livros
+let livros = await getDocs(collection(db,"livros"));
+selLivro.innerHTML = "<option>Livro</option>";
+livros.forEach(l=>{
+selLivro.innerHTML += `<option value="${l.id}">${l.data().nome}</option>`;
+});
+}
+
+document.getElementById("formEmprestimo").addEventListener("submit", async(e)=>{
+e.preventDefault();
+
+await addDoc(collection(db,"emprestimos"),{
+aluno: selAluno.options[selAluno.selectedIndex].text,
+livro: selLivro.options[selLivro.selectedIndex].text,
+status:"pendente"
+});
+
+carregar();
+});
+
+async function carregar(){
+
+let pend = document.getElementById("pendentes");
+let dev = document.getElementById("devolvidos");
+
+pend.innerHTML="";
+dev.innerHTML="";
+
+let dados = await getDocs(collection(db,"emprestimos"));
+
+dados.forEach(e=>{
+
+if(e.data().status === "pendente"){
+pend.innerHTML += `
+<tr>
+<td>${e.data().aluno}</td>
+<td>${e.data().livro}</td>
+<td>
+<button onclick="devolver('${e.id}')" class="btn btn-success btn-sm">
+Devolver
+</button>
+</td>
+</tr>
+`;
+}else{
+dev.innerHTML += `
+<tr>
+<td>${e.data().aluno}</td>
+<td>${e.data().livro}</td>
+</tr>
+`;
+}
+
+});
+
+}
+
+window.devolver = async(id)=>{
+await updateDoc(doc(db,"emprestimos",id),{
+status:"devolvido"
+});
+carregar();
+};
+
+carregarSelects();
+carregar();
