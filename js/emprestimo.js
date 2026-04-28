435e6f3f291d632cbf09f7a9e2cb4df69e21b443
anasousa123 <ana.7410062@aluno.mg.gov.br>
2026-04-28 10:26:04 -0300

    gg

--- js/emprestimo.js
+++ js/emprestimo.js
@@ -1,4 +1,4 @@
-import { db } from "./firebase.js";
+import { db } from "java.script";
 import {
 collection, addDoc, getDocs, updateDoc, doc
 } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-firestore.js";
