bd4b8bd4c39fe17a45bf19afe1ed2c9b575ac0cb
Yasmin <yasmin.9399978@aluno.mg.gov.br>
2026-04-28 10:41:26 -0300

    l

--- js/firebase.js
+++ js/firebase.js
@@ -8,9 +8,6 @@ const firebaseConfig = {
   apiKey: "AIzaSyClUVg-jBh6ufj0j-7nKZscvUiqIVnc0UA",
   authDomain: "biblioteca-60ede.firebaseapp.com",
   projectId: "biblioteca-60ede",
-  storageBucket: "biblioteca-60ede.firebasestorage.app",
-  messagingSenderId: "769030432325",
-  appId: "1:769030432325:web:2643471f8d722fe41d652c"
 };
 
 // Inicializa
