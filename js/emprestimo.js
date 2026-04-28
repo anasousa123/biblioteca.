c30e246b07ad09207b5d2ae4e8c2fbd8a33cc688
Yasmin <yasmin.9399978@aluno.mg.gov.br>
2026-04-28 10:39:33 -0300

    kk

--- js/emprestimo.js
+++ js/emprestimo.js
@@ -1,4 +1,4 @@
-import { db } from "java.script";
+import { db } from "./firebase.js";
 import {
 collection, addDoc, getDocs, updateDoc, doc
 } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-firestore.js";
