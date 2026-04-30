d586e9179896bf2781b44d48801e43046ba58280
Yasmin <yasmin.9399978@aluno.mg.gov.br>
2026-04-30 08:35:44 -0300

    klç

--- js/alunos.js
+++ js/alunos.js
@@ -1,81 +1,31 @@
-
 import { db } from "./firebase.js";
-import { collection, addDoc, getDocs, deleteDoc, doc } 
-from "https://www.gstatic.com/firebasejs/12.12.1/firebase-firestore.js";
+ import { collection, addDoc, getDocs, deleteDoc, doc } 
+ from "https://www.gstatic.com/firebasejs/12.12.1/firebase-firestore.js";
 
-console.log("JS FUNCIONANDO");
+  console.log("JS FUNCIONANDO"); const form = document.getElementById("formAluno"); 
+  console.log("FORM:", form); const tabela = document.getElementById("tabela");
+   //PEGAR CAMPOS
+   const nome = document.getElementById("nome");
+   const turma = document.getElementById("turma");
+   const nivel = document.getElementById("nivel"); const email = document.getElementById("email");
 
-const form = document.getElementById("formAluno");
-console.log("FORM:", form);
-
-const tabela = document.getElementById("tabela");
-
-// PEGAR CAMPOS
-const nome = document.getElementById("nome");
-const turma = document.getElementById("turma");
-const nivel = document.getElementById("nivel");
-const email = document.getElementById("email");
-
-// CADASTRAR
-form.addEventListener("submit", async (e)=>{
-  e.preventDefault();
-
+  // CADASTRAR
+   form.addEventListener("submit", async (e)=>{ e.preventDefault();
   console.log("CLIQUE DETECTADO");
-
-  try {
-    const docRef = await addDoc(collection(db,"alunos"),{
-      nome: nome.value,
-      turma: turma.value,
-      nivel: nivel.value,
-      email: email.value
-    });
-
-    console.log("SALVO COM ID:", docRef.id);
-    document.getElementById("msg").innerHTML = `
-  <div class="alert alert-success">
-    Aluno cadastrado com sucesso!
-  </div>
-`;
-
-    form.reset();
-    carregar();
-
-  } catch (erro) {
-  console.error("ERRO REAL:", erro);
-
-  document.getElementById("msg").innerHTML = `
-    <div class="alert alert-danger">
-      Erro: ${erro.message}
-    </div>
-  `;
-  }
-});
-
-// LISTAR
-async function carregar(){
-tabela.innerHTML = "";
-
-const dados = await getDocs(collection(db,"alunos"));
-
-dados.forEach((item)=>{
-tabela.innerHTML += `
-<tr>
-<td>${item.data().nome}</td>
-<td>${item.data().turma}</td>
-<td>${item.data().nivel}</td>
-<td>${item.data().email}</td>
-<td>
-<button onclick="remover('${item.id}')" class="btn btn-danger btn-sm">Excluir</button>
-</td>
-</tr>
-`;
-});
-}
-
-// EXCLUIR
-window.remover = async(id)=>{
-await deleteDoc(doc(db,"alunos",id));
-carregar();
-};
-
-carregar();
+  try { const docRef = await addDoc(collection(db,"alunos"),{ nome: nome.value, turma: turma.value, nivel: nivel.value, email: email.value });
+  console.log("SALVO COM ID:", docRef.id);
+   document.getElementById("msg").innerHTML = <div class="alert alert-success"> Aluno cadastrado com sucesso! </div> ;
+    form.reset(); carregar(); } catch (erro) { console.error("ERRO REAL:", erro);
+      alert ("ERRO: " + erro.message); } });
+        // LISTAR 
+  async function carregar(){ tabela.innerHTML = ""; const dados = await getDocs(collection(db,"alunos"));
+     dados.forEach((item)=>{ tabela.innerHTML += 
+     <tr> 
+      <td>${item.data().nome}</td> 
+      <td>${item.data().turma}</td> 
+      <td>${item.data().nivel}</td> 
+      <td>${item.data().email}</td> 
+      <td> <button onclick="remover('${item.id}')" class="btn btn-danger btn-sm">Excluir</button> </td> 
+      </tr> ; }); } 
+      // EXCLUIR
+       window.remover = async(id)=>{ await deleteDoc(doc(db,"alunos",id)); carregar(); }; carregar();
