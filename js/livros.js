cc7291c58343ed67c4ba77dcf5be5c9b801a7172
Yasmin <yasmin.9399978@aluno.mg.gov.br>
2026-04-30 07:39:39 -0300

    l

--- js/livros.js
+++ js/livros.js
@@ -1,56 +1,78 @@
-
 import { db } from "./firebase.js";
 import {
-collection, addDoc, getDocs, deleteDoc, doc
+  collection, addDoc, getDocs, deleteDoc, doc
 } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-firestore.js";
+
+console.log("JS LIVROS FUNCIONANDO");
 
 const form = document.getElementById("formLivro");
 const tabela = document.getElementById("tabelaLivros");
 
-// CADASTRAR
-form.addEventListener("submit", async (e)=>{
-e.preventDefault();
-
-await addDoc(collection(db,"livros"),{
+// PEGAR CAMPOS
 const idLivro = document.getElementById("idLivro");
 const nomeLivro = document.getElementById("nomeLivro");
 const autor = document.getElementById("autor");
 const genero = document.getElementById("genero");
 const exemplares = document.getElementById("exemplares");
-});
 
-form.reset();
-carregar();
+console.log("FORM LIVROS:", form);
+
+// CADASTRAR
+form.addEventListener("submit", async (e)=>{
+  e.preventDefault();
+
+  console.log("CLIQUE LIVRO DETECTADO");
+
+  try {
+    const docRef = await addDoc(collection(db,"livros"),{
+      id: idLivro.value,
+      nome: nomeLivro.value,
+      autor: autor.value,
+      genero: genero.value,
+      exemplares: exemplares.value
+    });
+
+    console.log("LIVRO SALVO:", docRef.id);
+    alert("Livro cadastrado!");
+
+    form.reset();
+    carregar();
+
+  } catch (erro) {
+    console.error("ERRO AO SALVAR LIVRO:", erro);
+    alert("Erro: " + erro.message);
+  }
 });
 
 // LISTAR
 async function carregar(){
-tabela.innerHTML = "";
+  tabela.innerHTML = "";
 
-const dados = await getDocs(collection(db,"livros"));
+  const dados = await getDocs(collection(db,"livros"));
 
-dados.forEach(l=>{
-tabela.innerHTML += `
-<tr>
-<td>${l.data().id}</td>
-<td>${l.data().nome}</td>
-<td>${l.data().autor}</td>
-<td>${l.data().genero}</td>
-<td>${l.data().exemplares}</td>
-<td>
-<button onclick="remover('${l.id}')" class="btn btn-danger btn-sm">
-Excluir
-</button>
-</td>
-</tr>
-`;
-});
+  dados.forEach((l)=>{
+    tabela.innerHTML += `
+    <tr>
+      <td>${l.data().id}</td>
+      <td>${l.data().nome}</td>
+      <td>${l.data().autor}</td>
+      <td>${l.data().genero}</td>
+      <td>${l.data().exemplares}</td>
+      <td>
+        <button onclick="remover('${l.id}')" class="btn btn-danger btn-sm">
+          Excluir
+        </button>
+      </td>
+    </tr>
+    `;
+  });
 }
 
 // EXCLUIR
 window.remover = async(id)=>{
-await deleteDoc(doc(db,"livros",id));
-carregar();
+  await deleteDoc(doc(db,"livros",id));
+  carregar();
 };
 
+// CARREGAR AO ABRIR
 carregar();
