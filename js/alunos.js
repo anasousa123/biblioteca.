20310860014bf9cc22cd51a3a1b8c2350087711d
Yasmin <yasmin.9399978@aluno.mg.gov.br>
2026-05-11 07:37:44 -0300

    sdf

--- js/alunos.js
+++ js/alunos.js
@@ -1,6 +1,5 @@
 import { db } from "./firebase.js";
 import {
-  import {
   collection,
   addDoc,
   getDocs,
@@ -8,6 +7,7 @@ import {
   doc,
   query,
   orderBy
+  from "https://www.gstatic.com/firebasejs/12.12.1/firebase-firestore.js";
 }
 
 console.log("VERSAO NOVA ALUNOS");
