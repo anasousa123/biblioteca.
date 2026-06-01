a26cb0d89fcc3bb44e99e75db92e07a089e830d8
Yasmin <yasmin.9399978@aluno.mg.gov.br>
2026-06-01 13:39:32 -0300

    sadf

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js";
 
-const supabaseUrl = "https://msdnrjegnbvqwrnfdwkq.supabase.co/rest/v1/";
+const supabaseUrl = "https://msdnrjegnbvqwrnfdwkq.supabase.co";
 const supabaseKey = "sb_publishable_BPDzB7rC76CJ4yuNLhIMXQ_WHhDT-eE";
 
 export const supabase = createClient(
