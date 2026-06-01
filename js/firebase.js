cbe60cc2fea1b71fc2552c36843e273b0c5393ec
Yasmin <yasmin.9399978@aluno.mg.gov.br>
2026-06-01 09:03:12 -0300

    yuk

--- js/firebase.js
+++ js/firebase.js
@@ -1,16 +1,9 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js";
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-firestore.js";
+const supabaseUrl = "https://msdnrjegnbvqwrnfdwkq.supabase.co/rest/v1/";
+const supabaseKey = "SUA_CHAVE_ANON";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCOsIklRS-PtT1kxHQ5My-CUgd1n7mzUnI",
-  authDomain: "teste-6f017.firebaseapp.com",
-  projectId: "teste-6f017",
-  storageBucket: "teste-6f017.firebasestorage.app",
-  messagingSenderId: "331373288049",
-  appId: "1:331373288049:web:0ca4d325d55a8e2df2d9d0"
-};
-
-const app = initializeApp(firebaseConfig);
-
-export const db = getFirestore(app);
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseKey
+);
