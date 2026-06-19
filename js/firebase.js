b73d3d9506ac286aa2b6af37da46c1383461391e
Yasmin <yasmin.9399978@aluno.mg.gov.br>
2026-06-19 08:26:28 -0300

    asdasd

--- js/firebase.js
+++ js/firebase.js
@@ -1,13 +1,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyClUVg-jBh6ufj0j-7nKZscvUiqIVnc0UA",
-  authDomain: "biblioteca-60ede.firebaseapp.com",
-  projectId: "biblioteca-60ede",
-  storageBucket: "biblioteca-60ede.firebasestorage.app",
-  messagingSenderId: "769030432325",
-  appId: "1:769030432325:web:2643471f8d722fe41d652c"
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_PROJETO.firebaseapp.com",
+  projectId: "SEU_PROJETO",
+  storageBucket: "SEU_PROJETO.appspot.com",
+  messagingSenderId: "XXXXXXXX",
+  appId: "XXXXXXXX"
 };
 
 const app = initializeApp(firebaseConfig);
