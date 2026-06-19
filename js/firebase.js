545d55c93b771febeb911630a212c627e2b88837
anasousa123 <ana.7410062@aluno.mg.gov.br>
2026-06-19 13:47:52 +0000

    99

--- js/firebase.js
+++ js/firebase.js
@@ -1,16 +1,15 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  projectId: "SEU_PROJETO",
-  storageBucket: "SEU_PROJETO.appspot.com",
-  messagingSenderId: "XXXXXXXX",
-  appId: "XXXXXXXX"
+  apiKey: "AIzaSyClUVg-jBh6ufj0j-7nKZscvUiqIVnc0UA",
+  authDomain: "biblioteca-60ede.firebaseapp.com",
+  projectId: "biblioteca-60ede",
+  storageBucket: "biblioteca-60ede.firebasestorage.app",
+  messagingSenderId: "769030432325",
+  appId: "1:769030432325:web:2643471f8d722fe41d652c",
+  measurementId: "G-HQGQQM4GRN"
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const db = getFirestore(app);
